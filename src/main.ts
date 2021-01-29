9bfed960239d36592d5d23f9fc1d7c47c9c929f3
Baelyk <b43lyk@gmail.com>
2021-01-29 17:52:55 -0600

    Add error wrappers to loop parts
    Add wrappers to parts of the loop so that one error in one part doesn't
    take down the whole loop. #2

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ export const loop = ErrorMapper.wrapLoop(() => {
   logger.tick();
 
   if (Memory.uninitialized) {
-    init();
+    wrapper(() => init(), `Error initializing`);
   }
 
   // Debug
@@ -27,8 +27,10 @@ export const loop = ErrorMapper.wrapLoop(() => {
   // Automatically delete memory of missing creeps
   for (const name in Memory.creeps) {
     if (!(name in Game.creeps)) {
-      handleDead(name);
-      delete Memory.creeps[name];
+      wrapper(() => {
+        handleDead(name);
+        delete Memory.creeps[name];
+      }, `Error handling death of creep ${name}`);
     }
   }
 
@@ -42,132 +44,147 @@ export const loop = ErrorMapper.wrapLoop(() => {
 
   // Process spawn behavior
   for (const name in Game.spawns) {
-    spawnManager(Game.spawns[name]);
+    wrapper(
+      () => spawnManager(Game.spawns[name]),
+      `Error processing spawn ${name} behavior`,
+    );
   }
 
   // Process room behavior
-  for (const roomName in Game.rooms) {
-    const room = Game.rooms[roomName];
-    if (room.memory.level >= 3) {
-      // Process tower behavior
-      for (const towerIndex in room.memory.towers) {
-        const tower = Game.getObjectById(
-          room.memory.towers[towerIndex],
-        ) as StructureTower;
-        if (tower !== null) {
-          towerManager(tower);
-        } else {
-          logger.warn(
-            `Unable to get tower of id ${room.memory.towers[towerIndex]} in room ${roomName}`,
-          );
+  wrapper(() => {
+    for (const roomName in Game.rooms) {
+      const room = Game.rooms[roomName];
+      if (room.memory.level >= 3) {
+        // Process tower behavior
+        for (const towerIndex in room.memory.towers) {
+          const tower = Game.getObjectById(
+            room.memory.towers[towerIndex],
+          ) as StructureTower;
+          if (tower !== null) {
+            wrapper(
+              () => towerManager(tower),
+              `Error processing tower ${tower.id} behavior`,
+            );
+          } else {
+            throw new GetByIdError(
+              room.memory.towers[towerIndex],
+              STRUCTURE_TOWER,
+              `The tower should be in room ${roomName}`,
+            );
+          }
+        }
+      }
+      if (room.memory.level >= 4) {
+        // Process link behavior
+        for (const linkId in room.memory.links.all) {
+          const link = Game.getObjectById(linkId) as StructureLink | null;
+          if (link != undefined) {
+            wrapper(
+              () => linkManager(link),
+              `Error processing link ${linkId} behavior`,
+            );
+          } else {
+            throw new GetByIdError(
+              linkId,
+              STRUCTURE_LINK,
+              `The link should be in room ${roomName}`,
+            );
+          }
         }
       }
     }
-    if (room.memory.level >= 4) {
-      // Process link behavior
-      for (const linkId in room.memory.links.all) {
-        const link = Game.getObjectById(linkId) as StructureLink | null;
-        if (link != undefined) {
-          linkManager(link);
-        } else {
-          throw new GetByIdError(
-            linkId,
-            STRUCTURE_LINK,
-            `The link should be in room ${roomName}`,
-          );
-        }
-      }
-    }
-  }
+  }, `Error processing room behavior`);
 
   // Infrequent actions:
   // Update repair queue and pop limits every 100 ticks
-  if (Game.time % 100 === 0) {
-    for (const name in Game.rooms) {
-      const room = Game.rooms[name];
-      const controller = room.controller;
-      // This will not work with multiple rooms, despite the way I've made it
-      resetRepairQueue(room);
-      updateWallRepair(room);
-      census(room);
-      // If we have reached the miner tier, queue as many containers as possible for sources
-      if (
-        !Memory.status.builtAllSourceContainers &&
-        Memory.populationLimit.miner
-      ) {
-        const maxExtensions = getMaxExtensions(
-          (room.controller as StructureController).level,
-        );
-        const extensionsCount = room
-          .find(FIND_MY_STRUCTURES)
-          .filter((structure) => {
-            return structure.structureType === STRUCTURE_EXTENSION;
-          }).length;
-        if (extensionsCount === maxExtensions) {
-          logger.info(`Requesting containers around sources`, InfoType.build);
-          // constructMinerContainers(room, -1);
-          Memory.status.builtAllSourceContainers = true;
-        } else {
-          logger.info(
-            `Waiting for max extensions to request containers around sources`,
-            InfoType.build,
+  wrapper(() => {
+    if (Game.time % 100 === 0) {
+      for (const name in Game.rooms) {
+        const room = Game.rooms[name];
+        const controller = room.controller;
+        // This will not work with multiple rooms, despite the way I've made it
+        resetRepairQueue(room);
+        updateWallRepair(room);
+        census(room);
+        // If we have reached the miner tier, queue as many containers as possible for sources
+        if (
+          !Memory.status.builtAllSourceContainers &&
+          Memory.populationLimit.miner
+        ) {
+          const maxExtensions = getMaxExtensions(
+            (room.controller as StructureController).level,
           );
-        }
-      }
-
-      if (controller !== undefined) {
-        // If the controller has leveled up, level up the room
-        if (controller.level !== room.memory.level) {
-          room.memory.level = controller.level;
-          logger.info(`Updating room memory level to ${room.memory.level}`);
-          if (room.memory.level === 3) {
-            // Level 3: Build tower
-            buildTower(room.name);
+          const extensionsCount = room
+            .find(FIND_MY_STRUCTURES)
+            .filter((structure) => {
+              return structure.structureType === STRUCTURE_EXTENSION;
+            }).length;
+          if (extensionsCount === maxExtensions) {
+            logger.info(`Requesting containers around sources`, InfoType.build);
+            // constructMinerContainers(room, -1);
+            Memory.status.builtAllSourceContainers = true;
+          } else {
+            logger.info(
+              `Waiting for max extensions to request containers around sources`,
+              InfoType.build,
+            );
           }
-          if (room.memory.level === 4) {
-            // Level 4: Build storage
-            buildStorage(room.name);
-          }
         }
 
-        // Level-based room checks
-        if (room.memory.level >= 3) {
-          if (
-            room.memory.towers === undefined ||
-            room.memory.towers.length === 0
-          ) {
-            // There should be a tower (or construction site)
-            room.memory.towers = getTowersInRoom(room);
-            if (room.memory.towers.length === 0) {
-              logger.warn(
-                `Room ${room.name} should have a tower but none were found`,
-              );
+        if (controller !== undefined) {
+          // If the controller has leveled up, level up the room
+          if (controller.level !== room.memory.level) {
+            room.memory.level = controller.level;
+            logger.info(`Updating room memory level to ${room.memory.level}`);
+            if (room.memory.level === 3) {
+              // Level 3: Build tower
+              buildTower(room.name);
+            }
+            if (room.memory.level === 4) {
+              // Level 4: Build storage
+              buildStorage(room.name);
             }
           }
-        }
-        if (room.memory.level >= 4) {
-          // Set primary storage
-          if (room.memory.storage === undefined) {
-            const storage = room
-              .find(FIND_MY_STRUCTURES)
-              .find(
-                (structure) => structure.structureType === STRUCTURE_STORAGE,
-              ) as StructureStorage | undefined;
-            if (storage !== undefined) {
-              logger.info(
-                `Setting storage ${storage.id} as room ${room.name} primary storage`,
-              );
-              room.memory.storage = storage.id;
-            } else {
-              logger.info(
-                `No candidate for primary storage for room ${room.name}`,
-              );
+
+          // Level-based room checks
+          if (room.memory.level >= 3) {
+            if (
+              room.memory.towers === undefined ||
+              room.memory.towers.length === 0
+            ) {
+              // There should be a tower (or construction site)
+              room.memory.towers = getTowersInRoom(room);
+              if (room.memory.towers.length === 0) {
+                logger.warn(
+                  `Room ${room.name} should have a tower but none were found`,
+                );
+              }
+            }
+          }
+          if (room.memory.level >= 4) {
+            // Set primary storage
+            if (room.memory.storage === undefined) {
+              const storage = room
+                .find(FIND_MY_STRUCTURES)
+                .find(
+                  (structure) => structure.structureType === STRUCTURE_STORAGE,
+                ) as StructureStorage | undefined;
+              if (storage !== undefined) {
+                logger.info(
+                  `Setting storage ${storage.id} as room ${room.name} primary storage`,
+                );
+                room.memory.storage = storage.id;
+              } else {
+                logger.info(
+                  `No candidate for primary storage for room ${room.name}`,
+                );
+              }
             }
           }
         }
       }
     }
-  }
+  }, `Error during infrequent actions`);
 
   // Debug testing for energy harvested
   debugEnergyHarvested();
