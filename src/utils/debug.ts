f2b274512387600fdd1d746a769b38621e95c0bb
Baelyk <b43lyk@gmail.com>
2021-05-10 22:23:59 -0500

    Updated src/utils/debug.ts

--- src/utils/debug.ts
+++ src/utils/debug.ts
@@ -1,82 +1,11 @@
 import { info, warn } from "utils/logger";
-import { census } from "population";
-import {
-  executePlan,
-  getExitWallsAndRamparts,
-  getExtensionSpots,
-  makePlan,
-} from "planner";
-import { remoteTargetAnalysis } from "rooms";
-import { resetConstructionQueue } from "construct";
 import { VisibleRoom } from "roomMemory";
 
 export function debugLoop(): void {
-  const spawn = Game.spawns[Memory.initialSpawn];
-
-  if (Memory.debug.resetExtensions) {
-    resetExtensionSpots();
-    Memory.debug.resetExtensions = false;
-  }
-
   if (Memory.debug.resetRoomMemory) {
     resetRoomMemory();
     Memory.debug.resetRoomMemory = false;
   }
-
-  if (Memory.debug.resetPopLimits) {
-    resetPopLimits();
-    Memory.debug.resetPopLimits = false;
-  }
-
-  if (Memory.debug.testWallPlanner) {
-    getExitWallsAndRamparts(spawn.room);
-    Memory.debug.testWallPlanner = false;
-  }
-
-  if (Memory.debug.resetPlanner) {
-    makePlan(spawn.room);
-    Memory.debug.resetPlanner = false;
-  }
-
-  if (Memory.debug.executePlan) {
-    executePlan(spawn.room);
-    Memory.debug.executePlan = false;
-  }
-}
-
-function resetMemory(): void {
-  warn("Reseting memory");
-  Memory.uninitialized = true;
-  Memory.initialSpawn = "Spawn1";
-  Memory.watch = {};
-  Memory.debug = {
-    log: {
-      infoSettings: {
-        build: true,
-        general: true,
-        idleCreep: true,
-        spawn: true,
-        task: true,
-      },
-    },
-  };
-}
-
-function resetExtensionSpots(): void {
-  warn("Resetting extensions");
-  const spawn = Game.spawns[Memory.initialSpawn];
-  // Reset spawn extensionSpots memory
-  spawn.memory.extensionSpots = getExtensionSpots(spawn.room);
-  // Reset list of (built) extensions
-  const extensions = spawn.room
-    .find(FIND_MY_STRUCTURES)
-    .filter((structure) => structure.structureType === STRUCTURE_EXTENSION)
-    .map((extension) => extension.pos);
-  const extensionSites = spawn.room
-    .find(FIND_MY_CONSTRUCTION_SITES)
-    .filter((structure) => structure.structureType === STRUCTURE_EXTENSION)
-    .map((extension) => extension.pos);
-  spawn.memory.extensions = extensions.concat(extensionSites);
 }
 
 function resetRoomMemory(): void {
@@ -84,17 +13,6 @@ function resetRoomMemory(): void {
   for (const roomName in Game.rooms) {
     const room = new VisibleRoom(roomName);
     room.updateMemory();
-  }
-}
-
-function resetPopLimits(): void {
-  warn("Resetting population limits");
-  for (const roomName in Game.rooms) {
-    const room = Game.rooms[roomName];
-    census(room);
-
-    // Only run once
-    break;
   }
 }
 
