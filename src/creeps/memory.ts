e313924bd964ff9535644038dd977362a48002a2
Baelyk <b43lyk@gmail.com>
2021-07-15 16:05:51 -0500

    Remove error-throwing assigned construction sites

--- src/creeps/memory.ts
+++ src/creeps/memory.ts
@@ -1,5 +1,6 @@
 import { MemoryError } from "utils/errors";
 import * as CreepInfoHelpers from "./helpers";
+import { warn } from "utils/logger";
 
 // Keep the generic CreepMemory in global
 declare global {
@@ -188,9 +189,18 @@ export class BuilderCreepInfo extends CreepInfo {
   }
 
   getAssignedConstruction(): ConstructionSite | undefined {
-    return CreepInfoHelpers.getAssignedById(
-      this.getMemory().assignedConstruction,
-    );
+    try {
+      return CreepInfoHelpers.getAssignedById(
+        this.getMemory().assignedConstruction,
+      );
+    } catch (error) {
+      warn(
+        `Removing assigned construction from Creep ${this.creepName} due to error:`,
+      );
+      warn(error.toString());
+      this.removeAssignedConstruction();
+    }
+    return undefined;
   }
 
   setAssignedConstruction(id: Id<ConstructionSite>): void {
