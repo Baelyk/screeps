08fec7cf6eaef4a6309e0020fb35f1bccf0d972f
Baelyk <b43lyk@gmail.com>
2022-01-08 11:29:14 -0600

    Bare-bones src/main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,9 @@
 import { ErrorMapper } from "utils/ErrorMapper";
 import { watcher } from "utils/watch-client";
-import { tick } from "utils/logger";
-import { TestClass } from "testing";
 
-console.log("- - - - RESTARTING - - - -");
+console.log("==> RESTARTING <==");
 export const loop = ErrorMapper.wrapLoop(() => {
-  tick();
-
-  const test = new TestClass();
-  console.log(test.count);
-  console.log(test.count++);
-  console.log(test.count);
-  test.count += 10;
-  console.log(test.count);
+  console.log(Game.time);
 
   // screeps-multimeter watcher
   watcher();
